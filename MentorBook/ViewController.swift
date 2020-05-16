35bb80c70f79af6a32058f0f37306b76ce410674
sen1000-byte <nishiwaki.chihiro@gmail.com>
2020-05-16 09:25:21 +0900

    ３以上になっても落ちないようにしたい

--- MentorBook/ViewController.swift
+++ MentorBook/ViewController.swift
@@ -9,10 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var mentorArray: [Mentor] = []
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var courseLabel: UILabel!
+    
+    var index: Int = 0
 
+    func setUI() {
+        imageView.image = mentorArray[index].getImage()
+        nameLabel.text = mentorArray[index].name
+        courseLabel.text = mentorArray[index].course
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mentorArray.append(Mentor(name: "ながた", imageName: "nagata.jpg", course: "iPhone"))
+        mentorArray.append(Mentor(name: "りょう", imageName: "ryo.jpg", course: "Unity"))
+        mentorArray.append(Mentor(name: "たいてぃ", imageName: "taithi.JPG", course: "WebS,WebD"))
+        setUI()
         // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    @IBAction func mae(){
+        index = index - 1
+        setUI()
+    }
+    
+    @IBAction func tugi() {
+        index = index + 1
+        setUI()
     }
 
 
